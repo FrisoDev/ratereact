c101bfdc9d8c3c39ee800eff997e9c53f609f38d
FrisoDev <frisodevstoker@gmail.com>
2017-11-13 20:02:38 +0100

    Ripped game, broekn redirects. Fix tonight if poss.

--- src/components/UI/Navigation.js
+++ src/components/UI/Navigation.js
@@ -6,10 +6,9 @@ import { push } from 'react-router-redux'
 import signOut from '../../actions/user/sign-out'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton'
-import GameIcon from 'material-ui/svg-icons/hardware/videogame-asset'
 import FlatButton from 'material-ui/FlatButton'
 
-const TITLE = 'Play Memory'
+const TITLE = 'Student Evaluation'
 
 class Navigation extends PureComponent {
   static propTypes = {
@@ -36,10 +35,10 @@ class Navigation extends PureComponent {
     return (
       <AppBar
         title={TITLE}
-        iconElementLeft={<IconButton onClick={this.goHome}><GameIcon /></IconButton>}
+        iconElementLeft={<IconButton onClick={this.goHome}></IconButton>}
         iconElementRight={signedIn ?
           <FlatButton label="Sign out" onClick={this.signOut.bind(this)} /> :
-          <FlatButton label="Sign up" onClick={this.signUp} />
+          null
         }
       />
     )
--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -5,7 +5,6 @@ import { replace, push } from 'react-router-redux'
 import Paper from 'material-ui/Paper'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
-import FlatButton from 'material-ui/FlatButton'
 import signIn from '../actions/user/sign-in'
 import Title from '../components/UI/Title'
 
@@ -58,9 +57,6 @@ export class SignIn extends PureComponent {
             <TextField ref="password" type="password" hintText="Password"  />
           </div>
         </form>
-        <FlatButton
-          onClick={ this.signUp.bind(this) }
-          label="Sign up" />
         <RaisedButton
           style={ buttonStyle }
           onClick={ this.submitForm.bind(this) }
